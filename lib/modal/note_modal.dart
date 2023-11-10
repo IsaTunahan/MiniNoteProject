842798751d56f28fc34abe961182b19148d4642d
IsaTunahan <118114545+IsaTunahan@users.noreply.github.com>
2023-11-10 23:29:35 +0300

    Created first folders

---
+++ lib/modal/note_modal.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class NoteMadel {
+  int id;
+  String content;
+
+  NoteMadel({
+    required this.id,
+    required this.content,
+  });
+}
---
+++ lib/utils/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class AppTheme {
+  //Colors
+  static const Color primaryColor = Colors.black;
+  static const Color accentColor = Colors.red;
+
+
+//Paddinga
+static const EdgeInsets defaultPadding = EdgeInsets.symmetric(horizontal: 16);
+
+}
